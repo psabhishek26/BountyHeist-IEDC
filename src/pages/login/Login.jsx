8220ed13465c76e165e8f6bc3f0b1ad8e62f9210
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2024-09-26 14:36:21 +0530

    register and login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,67 +1,58 @@
 import React from 'react';
 import './login.css';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input, Flex } from 'antd';
+import { Button, Checkbox, Form, Input, Row, Col } from 'antd';
+
 const Login = () => {
   const onFinish = (values) => {
     console.log('Received values of form: ', values);
   };
+
   return (
     <div className='loginmotham'>
-    <div className="headme">
+      <div className="headme">
         <h3>Bounty Heist</h3>
         <p>IEDC JECC</p>
-    </div>
-    <Form
-    className='theloginmenu'
-      name="login"
-      initialValues={{
-        remember: true,
-      }}
-      style={{
-        maxWidth: 360,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
-        ]}
+      </div>
+      <Form
+        className='theloginmenu'
+        name="login"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
       >
-        <Input prefix={<UserOutlined />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
-      </Form.Item>
-      <Form.Item>
-        <Flex justify="space-between" align="center">
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-          <a href="">Forgot password</a>
-        </Flex>
-      </Form.Item>
-
-      <Form.Item>
-        <Button block type="primary" htmlType="submit">
-          Log in
-        </Button>
-        or <a href="">Register now!</a>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: 'Please input your Username!' }]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Please input your Password!' }]}
+        >
+          <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+        </Form.Item>
+        <Form.Item>
+          <Row justify="space-between" align="middle">
+            <Col>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+            </Col>
+            <Col>
+              <a href="">Forgot password</a>
+            </Col>
+          </Row>
+        </Form.Item>
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Log in
+          </Button>
+          or <a href="/register">Register now!</a>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
+
 export default Login;
