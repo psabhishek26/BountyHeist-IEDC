7ddae5ee8fd6b33eeefc07651226db249b001e46
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-09-29 10:21:03 +0530

    Save user Auth status and prevent navigation to internal pages if user not signed in
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css';
-import {BrowserRouter as Router , Routes , Route, BrowserRouter} from 'react-router-dom';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import Home from './pages/Home/Home';
-import Landing from './pages/Landing/Landing';
-import ComingSoon from './pages/ComingSoon/ComingSoon';
+import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/Home/Home";
+import Landing from "./pages/Landing/Landing";
+import ComingSoon from "./pages/ComingSoon/ComingSoon";
+import PrivateRoute from "./components/PrivateRoute";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { useEffect, useContext } from "react";
+import { UserContext } from "./context/UserContext";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        const db = getDatabase();
+        const userRef = ref(db, "users/" + currentUser.uid);
+
+        onValue(userRef, (snapshot) => {
+          const data = snapshot.val();
+          if (data) {
+            setUser(data);
+          }
+        });
+      } else {
+        setUser(null);
+        navigate("/landing");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home/>} />
-          <Route path="/" element={<ComingSoon/>} />
-          <Route path="/register" element={<Register/>} />
-          <Route path='/login' element = {<Login/>} />
-          <Route path='/landing' element = {<Landing/>} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/coming" element={<ComingSoon />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
