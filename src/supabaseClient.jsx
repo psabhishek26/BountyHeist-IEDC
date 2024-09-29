2fd841160f307028ad406e2a0bdd23884b203145
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-09-29 10:11:47 +0530

    Initialize Firebase
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/supabaseClient.jsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // Replace with your Supabase URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // Replace with your Supabase Anon Key
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
