15bd385bc890f36b1164a5c8ecc6cd7acf6a2498
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-10-02 02:08:43 +0530

    Update coming soon message for event
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/pages/ComingSoon/ComingSoon.jsx
+++ src/pages/ComingSoon/ComingSoon.jsx
@@ -4,7 +4,7 @@ import './comingsoon.css';
 const ComingSoon = () => {
   return (
     <div className='comingsoon'> 
-    <h3>Starts Tommorow 09.00 am</h3>
+    <h3>Starts today at 9:00 AM</h3>
       
     </div>
 
